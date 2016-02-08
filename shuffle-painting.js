b8eb2627b5f656ae9ebebcd1f68df2a9b7325b98
Thiago Pontes <github@thiago.me>
2016-02-08 01:15:09 -0500

    second shuffle painting

--- shuffle-painting.js
+++ shuffle-painting.js
@@ -1,10 +1,13 @@
-var imageURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Gustav_Klimt_046.jpg/1280px-Gustav_Klimt_046.jpg";
+var imageURL = "./image.png";
 var img;
 var blockSizeX;
 var blockSizeY;
 var output;
+var d;
+
 function preload(){
-	img = loadImage(imageURL);	
+	img = loadImage(imageURL);
+    d = pixelDensity();
 }
 
 function setup(){
@@ -14,15 +17,13 @@ function setup(){
 	blockSizeY = img.height/5;
 	var areas = [];
 	var images = [];
-	for(var x = 0; x<img.width; x+=blockSizeX){
-		for(var y = 0; y<img.height; y+=blockSizeY){
-			images.push(img.get(x, y, blockSizeX, blockSizeY));
-			areas.push({x:x, y:y});
-		}
-	}	
-	shuffle(areas, true);
+    for (var i = 0; i < img.pixels.length ; i+=4) {
+        var r = Math.floor(Math.random() * img.pixels.length) % (i + 1);
+        var old = img.pixels[i];
+        img.pixels[i] = img.pixels[r];
+        img.pixels[r] = old;
+    }
 
-	for(var i = 0 ; i<images.length; i++){
-		image(images[i], areas[i].x, areas[i].y);
-	}
+    img.updatePixels();
+    image(img, 0, 0)
 }
